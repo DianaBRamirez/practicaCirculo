bc6c2fb950bbfc3aa1746cbe21392317cd862857
DianaBRamirez <dianabramirez29@gmail.com>
2024-02-08 18:39:17 -0600

    aparecer/desaparecer

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,43 @@ function cambiarColor() {
     var colorSeleccionado = select.value;
     circulo.style.backgroundColor = colorSeleccionado;
 }
-function agrandar() {
-    var circulo = document.getElementById("circulo");
-    var estilo = window.getComputedStyle(circulo);
-    var diametroActual = parseInt(estilo.width, 10);
-    circulo.style.width = (diametroActual + 40) + "px";
-    circulo.style.height = (diametroActual + 40) + "px";
+
+function agrandarWidth() {
+  var circulo = document.getElementById("circulo");
+  var estilo = window.getComputedStyle(circulo);
+  var anchoActual = parseInt(estilo.width, 10);
+  circulo.style.width = (anchoActual + 40) + "px";
+}
+
+function disminuirWidth() {
+  var circulo = document.getElementById("circulo");
+  var estilo = window.getComputedStyle(circulo);
+  var anchoActual = parseInt(estilo.width, 10);
+  if (anchoActual > 10) {
+    circulo.style.width = (anchoActual - 40) + "px";
   }
-  
-  function disminuir() {
-    var circulo = document.getElementById("circulo");
-    var estilo = window.getComputedStyle(circulo);
-    var diametroActual = parseInt(estilo.width, 10);
-    if (diametroActual > 10) {
-      circulo.style.width = (diametroActual - 40) + "px";
-      circulo.style.height = (diametroActual - 40) + "px";
-    }
+}
+
+function agrandarHeight() {
+  var circulo = document.getElementById("circulo");
+  var estilo = window.getComputedStyle(circulo);
+  var altoActual = parseInt(estilo.height, 10);
+  circulo.style.height = (altoActual + 40) + "px";
+}
+
+function disminuirHeight() {
+  var circulo = document.getElementById("circulo");
+  var estilo = window.getComputedStyle(circulo);
+  var altoActual = parseInt(estilo.height, 10);
+  if (altoActual > 10) {
+    circulo.style.height = (altoActual - 40) + "px";
+  }
+}
+
+  function ocultar() {
+    document.getElementById("circulo").hidden = true;
+  }
+
+  function mostrar() {
+    document.getElementById("circulo").hidden = false;
   }
